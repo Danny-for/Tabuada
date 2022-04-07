7cec83bbd01ae8cd166ac3dffeb0b0f540d3d744
danny-for <dannygeovanna2019@gmail.com>
2022-04-07 08:11:45 -0300

    @Danny-for [Tabuada]

---
+++ tabuada/tabuada/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int valor, num;
+            Console.WriteLine("Coloque o número para ter a tabuada : ");
+
+            valor = int.Parse(Console.ReadLine());
+
+            for (num = 1; num <= 10; num++)
+            {
+                Console.WriteLine($"{num} X {valor} = {num * valor}");
+            }
+
+
+        }
+    }
+}
